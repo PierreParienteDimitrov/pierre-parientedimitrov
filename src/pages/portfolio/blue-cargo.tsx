7385b94e52e2ae3c04ce81c1b498504fc86dd9dd
Pierre <pierre.parientedimitrov@gmail.com>
2022-06-01 21:56:33 -0700

    changes

--- src/pages/portfolio/blue-cargo.tsx
+++ src/pages/portfolio/blue-cargo.tsx
@@ -1,41 +1,11 @@
-import { pageInformation, Routes } from 'constants/pages'
-import { GetServerSideProps } from 'next'
-import { Session } from 'next-auth'
-import { getSession } from 'next-auth/react'
+imoprt {getServerSideProps} from '../../utils/protectedRoutes.ts'
 import React from 'react'
 
 const Bluecargo: React.FC = () => {
   return <div>Hello</div>
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session: Session | null = await getSession(context)
 
-  const hasAccess = session?.user.has_access
-
-  const query = pageInformation[Routes.BlueCargo]!.path
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: `${pageInformation[Routes.SignIn]!.path}?path=${query}`,
-        permanent: false,
-      },
-    }
-  }
-  if (!hasAccess) {
-    return {
-      redirect: {
-        destination: '/thank-you',
-        permanent: false,
-      },
-    }
-  }
-  return {
-    props: {
-      session,
-    },
-  }
-}
+export getServerSideProps
 
 export default Bluecargo
