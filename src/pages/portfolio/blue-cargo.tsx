44bd2571b8d14621356143e8ed53c4feb718c195
Pierre <pierre.parientedimitrov@gmail.com>
2022-06-01 21:59:14 -0700

    protected route refactor

--- src/pages/portfolio/blue-cargo.tsx
+++ src/pages/portfolio/blue-cargo.tsx
@@ -1,11 +1,10 @@
-imoprt {getServerSideProps} from '../../utils/protectedRoutes.ts'
 import React from 'react'
+
+import { getServerSideProps } from '@/utils/protectedRoutes'
 
 const Bluecargo: React.FC = () => {
   return <div>Hello</div>
 }
 
-
-export getServerSideProps
-
 export default Bluecargo
+export { getServerSideProps }
