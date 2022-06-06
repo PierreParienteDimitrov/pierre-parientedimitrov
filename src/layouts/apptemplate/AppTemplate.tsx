b821082cb10bdaa2b668c6c5d35612abc47be9e9
Pierre <pierre.parientedimitrov@gmail.com>
2022-06-06 09:13:00 -0700

    Footer (#33)
    * footer, overflow-x hidden and useSession AppPageTemplate"
    
    * comment
    
    * readme

--- src/layouts/apptemplate/AppTemplate.tsx
+++ src/layouts/apptemplate/AppTemplate.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/react'
 import React, { useState } from 'react'
 import { useEffect } from 'react'
+import { AiFillDribbbleCircle, AiFillLinkedin } from 'react-icons/ai'
 import { ILayoutProps } from 'types/ILayoutProps'
 
 import Container from '../containers/Container'
@@ -11,17 +12,19 @@ import Container from '../containers/Container'
 const AppTemplate = ({ children }: ILayoutProps) => {
   const router = useRouter()
   const pathname = router.pathname
-  const session = useSession()
+  const { data: session, status } = useSession()
+  const isUser = !!session?.user
   const [displayLogin, setDisplayLogin] = useState<boolean>(false)
 
   useEffect(() => {
-    if (session.status === 'unauthenticated') {
+    if (status === 'loading') return
+    if (!isUser) {
       setDisplayLogin(true)
     }
-  }, [])
+  }, [isUser, status])
 
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <nav className="relative z-50 flex h-12 w-full items-center justify-start bg-dark text-white">
         <div className="absolute bottom-0 h-[1.5px] w-full bg-gradient-to-r from-secondary to-tertiary"></div>
         <Container>
@@ -41,11 +44,6 @@ const AppTemplate = ({ children }: ILayoutProps) => {
               </li>
             </div>
             <div className="flex space-x-16">
-              <li>
-                <Link href="/contact" passHref>
-                  Contact Me
-                </Link>
-              </li>
               {displayLogin && (
                 <li>
                   <Link href={pageInformation[Routes.SignIn]!.path}>Login</Link>
@@ -56,6 +54,26 @@ const AppTemplate = ({ children }: ILayoutProps) => {
         </Container>
       </nav>
       {children}
+      <div className="mt-16 flex w-full flex-col items-center justify-center space-y-2 border-t py-8">
+        <h6>©Pierre Pariente Dimitrov Portfolio {new Date().getFullYear()}</h6>
+        <h6>All rights reserved</h6>
+        <div className="flex space-x-4">
+          <a
+            href="https://www.linkedin.com/in/pierre-pariente-97835742/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillLinkedin size={32} className="text-primary" />
+          </a>
+          <a
+            href="https://dribbble.com/Pariente"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillDribbbleCircle size={32} className="text-secondary" />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
