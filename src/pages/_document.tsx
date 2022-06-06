5e1a5072e74bac097ec941b81febdcd9e3b3249d
Pierre <pierre.parientedimitrov@gmail.com>
2022-06-06 13:25:58 -0700

    fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,18 +17,18 @@ class MyDocument extends Document {
               }}();`,
             }}
           />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          {/* @ts-ignore */}
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-            rel="stylesheet"
-          />
         </Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* @ts-ignore */}
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
         <body>
           <Main />
           <NextScript />
