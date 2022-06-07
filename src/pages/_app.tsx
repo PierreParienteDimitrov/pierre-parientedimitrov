5548be9d6ee6aef9f110383c76f6ec7d0e4dbc2a
Pierre <pierre.parientedimitrov@gmail.com>
2022-06-07 08:12:44 -0700

    segment local key (#39)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,11 +14,11 @@ Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
-const DEFAULT_WRITE_KEY = process.env.NEXT_PUBLIC_SEGMENT_KEY
+const DEFAULT_WRITE_KEY = process.env.NEXT_PUBLIC_DEFAULT_WRITE_KEY
 
 function renderSnippet() {
   const opts = {
-    apiKey: process.env.NEXT_PUBLIC_SEGMENT_KEY || DEFAULT_WRITE_KEY,
+    apiKey: process.env.NEXT_PUBLIC_ANALYTICS_WRITE_KEY || DEFAULT_WRITE_KEY,
     // note: the page option only covers SSR tracking.
     // Page.js is used to track other events using `window.analytics.page()`
     page: true,
@@ -28,7 +28,7 @@ function renderSnippet() {
     return snippet.max(opts)
   }
 
-  return snippet.max(opts)
+  return snippet.min(opts)
 }
 
 const MyApp = ({
