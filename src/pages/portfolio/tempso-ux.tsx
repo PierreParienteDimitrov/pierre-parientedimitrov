e6464d6939546051cbed14f7e463ce7ec5be0e07
Pierre <pierre.parientedimitrov@gmail.com>
2022-06-22 09:14:33 -0700

    linter

--- src/pages/portfolio/tempso-ux.tsx
+++ src/pages/portfolio/tempso-ux.tsx
@@ -1,7 +1,6 @@
 import { pageInformation, Routes } from 'constants/pages'
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/link'
 import React from 'react'
 
 import { ICarousel } from '@/../types/ICarousel'
