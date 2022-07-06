ef27d3f47e62ea5886cdec7e744f0a91b8c1d5b8
Pierre <pierre.parientedimitrov@gmail.com>
2022-07-05 23:13:43 -0700

    bug

--- src/utils/protectedRoutes.ts
+++ src/utils/protectedRoutes.ts
@@ -1,48 +1,47 @@
-import { pageInformation, Routes } from 'constants/pages'
 import { GetServerSideProps } from 'next'
 import { Session } from 'next-auth'
 import { getSession } from 'next-auth/react'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session: Session | null = await getSession(context)
-  const params = context.resolvedUrl
+  // const params = context.resolvedUrl
 
   console.log(session)
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: `${
-          pageInformation[Routes.SignIn]!.path
-        }?project=${params}`,
-        permanent: false,
-      },
-    }
-  }
+  // if (!session) {
+  //   return {
+  //     redirect: {
+  //       destination: `${
+  //         pageInformation[Routes.SignIn]!.path
+  //       }?project=${params}`,
+  //       permanent: false,
+  //     },
+  //   }
+  // }
 
-  if (session) {
-    interface UserSession {
-      user?:
-        | {
-            email: string
-            name: string
-            hasAccess: boolean
-          }
-        | undefined
-    }
+  // if (session) {
+  //   interface UserSession {
+  //     user?:
+  //       | {
+  //           email: string
+  //           name: string
+  //           hasAccess: boolean
+  //         }
+  //       | undefined
+  //   }
 
-    const userSession: UserSession = session.session as object
-    const hasAccess: boolean = userSession?.user?.hasAccess as boolean
+  //   const userSession: UserSession = session.session as object
+  //   const hasAccess: boolean = userSession?.user?.hasAccess as boolean
 
-    if (!hasAccess) {
-      return {
-        redirect: {
-          destination: '/thank-you',
-          permanent: false,
-        },
-      }
-    }
-  }
+  //   if (!hasAccess) {
+  //     return {
+  //       redirect: {
+  //         destination: '/thank-you',
+  //         permanent: false,
+  //       },
+  //     }
+  //   }
+  // }
 
   return {
     props: {
