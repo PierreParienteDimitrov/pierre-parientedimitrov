5f88cfd9df1c6e18695dbe18675147621bb45549
Pierre <pierre.parientedimitrov@gmail.com>
2022-07-06 08:28:01 -0700

    w-full

--- src/pages/portfolio/checkout-flow.tsx
+++ src/pages/portfolio/checkout-flow.tsx
@@ -225,7 +225,7 @@ const Content: React.FC = () => {
           </div>
         </div>
         {/* Wireframing */}
-        <div className="flex flex-col space-y-2">
+        <div className="flex w-full flex-col space-y-2">
           <h4 className="font-bold">Wireframing</h4>
           <div className="flex flex-col space-y-4">
             <p>
