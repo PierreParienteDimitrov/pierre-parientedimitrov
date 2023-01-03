bafee0f9adb2433d3fcff0702a46082d2d039133
Pariente Dimitrov <pierre.parientedimitrov@gmail.com>
2023-01-03 21:32:20 +0100

    layout tabs

--- src/utils/copy/uxMenuItems.ts
+++ src/utils/copy/uxMenuItems.ts
@@ -4,24 +4,32 @@ export const uxMenuItems = [
     src: '/user-research.svg',
     alt: 'user research icon',
     title: 'user research',
+    description:
+      'I conduct interviews to define personas and better understand the emotional triggers of the users.',
   },
   {
     filter: 'flowCharts',
     src: '/flow-charts.svg',
     alt: 'flow charts icon',
     title: 'flow charts',
+    description:
+      'I build flow charts to lay down the user journey and avoid dead-ends.',
   },
   {
     filter: 'architecture',
     src: '/architecture.svg',
     alt: 'information architecture icon',
     title: 'information architecture',
+    description:
+      'I create information architecture charts to prioritize user actions and organize an action funnel.',
   },
   {
     filter: 'wireframes',
     src: '/wiframes.svg',
     alt: 'wireframe icon',
     title: 'wireframes & prototypes',
+    description:
+      'I iterate quickly with engineering and product teams on wireframes to test ideas and scope projects.',
   },
 ]
 
--- types/IMethodology.ts
+++ types/IMethodology.ts
@@ -3,6 +3,7 @@ export interface IMethodology {
   src: string
   alt: string
   title: string
+  description: string
 }
 
 export interface IMethdologyTitle {
