c435821d0a173fa3a159613c01deed66e7728ea6
Pariente Dimitrov <pierre.parientedimitrov@gmail.com>
2023-04-05 18:20:34 -0700

    updated resume

--- src/utils/copy/aboutMe.ts
+++ src/utils/copy/aboutMe.ts
@@ -1,41 +1,55 @@
 export const aboutMe =
-  'I am a Product Designer and Front-End Engineer. For the last 18 month I have designed and helped built a software for the Supply Chain industry. I have previously freelanced for a variety of clients, and built a classical music streaming App for iOS and Android.'
+  "As a seasoned product designer with strong front-end engineering abilities, I offer a unique combination of technical and creative skills to my work. I have been the founding product designer at Tempso since 2017, where I co-created a classical music streaming platform integrated with Spotify and Apple Music. I have also been the head of product design at BlueCargo since 2021, contributing to the company's impressive growth in the Supply Chain software market, with revenue multiplied by 10 between 2021 and 2023. Additionally, I serve as a lead product designer contractor for Charter Communications (Spectrum), where I have built the TVSDK design systems."
 
 export const skills = [
-  'Interaction Design: Figma, Sketch, Adobe XD. Strong experience in building and managing large component library and documentation for developers.',
-  'User Experience Research: Experienced User Researcher with many usability interviews conducted for a wide range of digital products.',
-  'Engineering language and framework (intermediate skills): HTML, CSS, JavaScript, Typescript, React.js, Next.js, Node.js, Express, MongoDB, SQL databases.',
+  'User Research (Expert): Skilled user researcher with numerous usability interviews and industry analyses for a broad range of digital products, including persona development and empathy mapping.',
+  'User Experience (Expert): Strong abilities in developing user flows, data architecture maps, and wireframes.',
+  'UI Design (Expert): Extensive experience in creating UI components for various libraries with diverse styles and brand identities.',
+  'Design System (Expert): Proven experience in building and managing complex, fast-growing cross-team component libraries.',
+  'UX/UI Tools (Expert): Proficient in Figma, FigJam, Sketch, Adobe XD, mind mapping and site architecture tools.',
+  'Front-end Engineering (Intermediate): HTML, CSS, Tailwind, MUI, JavaScript, Typescript, React.js, and Next.js.',
+  'Back-end Engineering (Junior): Node.js, Express, MongoDB, and SQL databases.',
 ]
 
 export const professionalExperiences = [
   {
-    position: 'Product Designer - UX/UI Engineer',
+    position: 'Lead Product Designer for video platform systems design',
+    dates: 'Sept 22 - Today',
+    project: 'Charter Communications (Spectrum)',
+    city: 'Remote',
+    description: [
+      'Successfully transitioned all design assets from Sketch to Figma. ',
+      'Built and maintained a comprehensive component library and design system for all video platforms including Apple TV, Roku, Xbox, Samsung TVs, and mobile devices.',
+      'Collaborated with multiple teams to deliver a design system that meets the needs of all stakeholders.',
+    ],
+  },
+  {
+    position: 'Head of Product Design',
     dates: 'Feb 21 - Today',
-    project: 'BlueCargo - saas - web app',
+    project: 'BlueCargo - SaaS - web app',
     city: 'Los Angeles',
     description: [
       'Conducted UX research in the Drayage Industry to identify customers paint points, current workflows, identify personas and  product opportunities.',
       'Built wireframes to visualise user flows and the steps that customers need to perform to achieve a task.',
-      'Designed high-fidelity mockups and complex component library to share with the engineering team.',
-      'Developed front-end component in React.js with the engineering team and built a shared component library between the product design and the front-end.',
-      'Implemented analytics plan, implemented third-party tools like Segment and defined products KPIs. ',
+      'Designed high-fidelity mockups and complex component library to share with the engineering team and built numerous features including a dashboard; homepage; container tracking table and filter experience; sign up and sign flows; navigation; checkout flow; etc.',
+      'Developed some front-end component in React.js and Typescript with the engineering team and built a shared component library between the product design and the front-end. ',
+      'Implemented analytics plan with third-party tools like Segment and defined products KPIs. ',
     ],
   },
   {
-    position: 'CO-FOUNDER & HEAD OF PRODUCT DESIGN',
+    position: 'Co-Founder - Product Designer',
     dates: 'Sept 20 - Today',
     company: 'Tempso',
-    project: 'Classical Music Streaming Add-on for Spotify - Mobile App',
-    city: 'Los Angeles - Paris',
+    project: 'Classical Music Streaming App for Spotify',
+    city: 'Remote',
     description: [
-      'Tempso is a passion project.',
       'Conducted UX research to narrow-down personas and product opportunities.',
       'Built wireframes to visualise user flows and define how users move through the classical music catalog.',
       'Designed high-fidelity mockups and complex component library for each screens of the App.',
     ],
   },
   {
-    position: 'FREELANCER',
+    position: 'Freelancer',
     dates: 'Jan 20 - Feb 21',
     city: 'Los Angeles - Paris',
     description: [
@@ -46,10 +60,10 @@ export const professionalExperiences = [
     ],
   },
   {
-    position: 'CHIEF MARKETING OFFICER & BUSINESS DEVELOPMENT',
+    position: 'Chief Marketing Officer - Business Development',
     dates: 'Sept 17 - Apr 21',
-    company: 'DELAIR LABS',
-    project: 'Cinema Sound Technology Company - 2019 HPA Innovation Winner',
+    company: 'Delair Labs',
+    project: 'Cinema Sound Technology - 2019 HPA Innovation Winner',
     city: 'Los Angeles - Paris',
     description: [
       'Lead Business Development efforts and generated a sales pipeline of qualified leads for $500k; Developed relationships with Hollywood Professional Association and Society of Motion Picture and Television Engineers.',
@@ -60,7 +74,7 @@ export const professionalExperiences = [
     ],
   },
   {
-    position: 'STRATEGY CONSULTANT',
+    position: 'Strategy Consultant',
     dates: 'Oct 14 - Jul 17',
     company: 'BNP Paribas - Bank of the West',
     city: 'San Francisco',
