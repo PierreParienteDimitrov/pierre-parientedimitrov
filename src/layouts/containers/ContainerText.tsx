15f351c6c7599412698afaa2d918f34c22b5d5f3
Pariente Dimitrov <pierre.parientedimitrov@gmail.com>
2023-04-13 18:45:31 -0700

    big paragraphs

--- src/layouts/containers/ContainerText.tsx
+++ src/layouts/containers/ContainerText.tsx
@@ -1,7 +1,7 @@
 import { ILayoutProps } from 'types/ILayoutProps'
 
 const ContainerText = ({ children }: ILayoutProps) => {
-  return <div className="m-auto w-full lg:w-7/12 2xl:w-6/12">{children}</div>
+  return <div className="m-auto w-full lg:w-8/12 2xl:w-8/12">{children}</div>
 }
 
 export default ContainerText
