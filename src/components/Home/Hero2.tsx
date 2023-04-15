8a4ec5cb20bdecb28cad4dde10c86eba958b5303
Pierre <pierre.parientedimitrov@gmail.com>
2023-04-14 19:06:57 -0700

    fade in animation (#78)

--- src/components/Home/Hero2.tsx
+++ src/components/Home/Hero2.tsx
@@ -4,25 +4,19 @@ import React from 'react'
 
 import Container from '@/layouts/containers/Container'
 
-import styles from '../../styles/blink.module.css'
-import shineEffect from '../../styles/shineEffect.module.css'
+import blink from '../../styles/blink.module.css'
+import fadeIn from '../../styles/fadeIn.module.css'
 
 const Hero2 = () => {
   return (
     <Container>
       <div className="flex w-full flex-col items-center justify-start space-y-2 overflow-hidden pb-24 dark:text-gray-100 md:order-1 md:flex-row md:pt-32 md:pb-48">
         <div className="order-2 flex flex-col space-y-10 md:w-4/6 md:pr-44">
-          <h1 className="font-medium text-gray-400">
+          <h1 className={`font-medium text-gray-400 ${fadeIn['fade-in-text']}`}>
             Hi, I&apos;m Pierre - a{' '}
-            <span className={`${shineEffect.textBlue} italic text-white`}>
-              product designer
-            </span>{' '}
-            and
-            <span className={`${shineEffect.textRed} italic text-white`}>
-              {' '}
-              software engineer
-            </span>
-            , with affinities for purpose-driven products.
+            <span className={`italic text-white`}>product designer</span> and
+            <span className={`italic text-white`}> software engineer</span>,
+            with affinities for purpose-driven products.
           </h1>
         </div>
         <Link href="/resume" passHref>
@@ -37,7 +31,7 @@ const Hero2 = () => {
               className="z-10 rounded-full shadow-lg"
             />
             <div
-              className={`${styles.blink} absolute bottom-16 left-0 h-[260px] w-[260px] rounded-full bg-primary-100 dark:bg-white md:bottom-10 md:left-4 md:h-[300px] md:w-[300px] lg:h-[450px] lg:w-[450px]`}
+              className={`${blink.blink} absolute bottom-16 left-0 h-[260px] w-[260px] rounded-full bg-primary-100 dark:bg-white md:bottom-10 md:left-4 md:h-[300px] md:w-[300px] lg:h-[450px] lg:w-[450px]`}
             ></div>
           </div>
         </Link>
