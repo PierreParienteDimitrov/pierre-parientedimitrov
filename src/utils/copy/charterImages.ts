551bf46418d949bdff9ef3b896cbba15faa0ece0
Pariente Dimitrov <pierre.parientedimitrov@gmail.com>
2023-04-16 14:45:14 -0700

    responsive for charter

---
+++ src/utils/copy/charterImages.ts
@@ -0,0 +1,38 @@
+export const charterImages = [
+  {
+    src: '/spectrum-movie.png',
+    alt: 'spectrum movie',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    src: '/spectrum-related.png',
+    alt: 'spectrum related',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    src: '/spectrum-channel.png',
+    alt: 'spectrum channel',
+    width: 1280,
+    height: 720,
+  },
+  {
+    src: '/spectrum-checkout.png',
+    alt: 'spectrum home',
+    width: 1280,
+    height: 720,
+  },
+  {
+    src: '/spectrum-preferences.png',
+    alt: 'spectrum preference',
+    width: 1280,
+    height: 720,
+  },
+  {
+    src: '/spectrum-ondemand.png',
+    alt: 'spectrum on demand',
+    width: 1280,
+    height: 720,
+  },
+]
